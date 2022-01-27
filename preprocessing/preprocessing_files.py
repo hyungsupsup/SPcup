7f8236b9b7fed8ac60a9145c2e24a840dc5249db
hyungsup <84906047+hyungsupsup@users.noreply.github.com>
2022-01-27 15:47:59 +0900

    원하는 이름의 파일 특정 개수만큼 이동시키기

--- preprocessing/preprocessing_files.py
+++ preprocessing/preprocessing_files.py
@@ -1,24 +1,25 @@
-import os,glob,random,shutil
-from pathlib import Path
-dir_path=Path('F:/건강관리를 위한 음식 이미지/data4training') #abc 폴더 경로
-parent_folder=os.path.join(str(dir_path.parent)+"/Validation") #abc의 부모폴더 +validation
 
 
-for dir in os.listdir(dir_path):
-    if not dir.endswith("json"):
-        path=os.path.join(str(dir_path)+'/'+dir) #abc폴더내 이미지 폴더주소 순회
-        file_list = os.listdir(path)
-        json_folder=os.path.join(parent_folder+'/'+dir+' json') #validation폴더내 json 폴더 경로
-        final_path = os.path.join(path + " json")
-        os.makedirs(final_path) #abc 폴더내 새로운 json폴더 만들어줌
-        for file in file_list:
-            file=file[:-4]
+#src = 'C:/Users/TCLAB-JG/Desktop/spcup_2022/audiofile'
+#dir = 'C:/Users/TCLAB-JG/Desktop/spcup_2022/audiofile_shorted500'
 
-            try:
-                shutil.copy(json_folder+"/"+file+".json", final_path)
+import shutil
+import os
 
-            except FileNotFoundError:
-                pass
-    else:
-        pass
+source = 'C:/Users/TCLAB-JG/Desktop/spcup_2022/audiofile - 복사본'
+dest1 = 'C:/Users/TCLAB-JG/Desktop/spcup_2022/audiofile_shorted500'
 
+
+f_name=None
+for n in range(0,5):
+    for f_n in range(1,101):
+        f_name=str(n)+'_'+str(f_n)+'.wav'
+        shutil.move(source + '/' + f_name, dest1)
+
+# for f in files:
+#     if f.startswith('0_'):
+#         for i in range(0,100):
+#             shutil.move(source+'/'+files[i],dest1)
+#     if f.startswith('1_'):
+#         for j in range(0, 100):
+#             shutil.move(source + '/' + files[j], dest1)
